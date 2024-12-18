1a784a84ccfe33600e039074f57fb9874523ac0b
monasubedi <sbd.mona@gmail.com>
2024-12-18 16:50:34 -0600

    api binding for tables

--- backend-restaurant-setup/src/main/java/com/restaurantsetup/controller/BookingTableController.java
+++ backend-restaurant-setup/src/main/java/com/restaurantsetup/controller/BookingTableController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -19,12 +20,13 @@ import com.restaurantsetup.dto.BookingTableResponse;
 import com.restaurantsetup.service.BookingTableService;
 
 @RestController
-@RequestMapping("/booking-tables")
+@RequestMapping("/tables")
+@PreAuthorize("hasRole('ROLE_MANAGER')")
 public class BookingTableController {
     @Autowired
     private BookingTableService bookingTableService;
 
-    @PostMapping("/create")
+    @PostMapping("/")
     public ResponseEntity<?> createBookingTable(@RequestBody BookingTableRequest bookingTableRequest) {
         BookingTableResponse bookingTableResponse = bookingTableService.createBookingTable(bookingTableRequest);
         return new ResponseEntity<BookingTableResponse>(bookingTableResponse, HttpStatus.CREATED);
--- frontend/src/context/AuthContext.tsx
+++ frontend/src/context/AuthContext.tsx
@@ -1,53 +1,61 @@
-import { createContext, useState, useContext, ReactNode, useEffect } from 'react';
-import { useNavigate } from 'react-router-dom';
+import {
+  createContext,
+  ReactNode,
+  useContext,
+  useEffect,
+  useState,
+} from "react";
 
 interface AuthContextType {
-    isLoggedIn: boolean;
-    user: { username: string, token: string, role: Role } | null;
-    login: (username: string, token: string, role: Role) => void;
-    logout: () => void
+  isLoggedIn: boolean;
+  user: { username: string; token: string; role: Role } | null;
+  login: (username: string, token: string, role: Role) => void;
+  logout: () => void;
 }
 
-export type Role = 'manager' | 'staff' | 'customer';
+export type Role = "manager" | "staff" | "customer";
 
 const AuthContext = createContext<AuthContextType | undefined>(undefined);
 
 export const AuthProvider = ({ children }: { children: ReactNode }) => {
-    const [isLoggedIn, setIsLoggedIn] = useState(false);
-    const [user, setUser] = useState<{ username: string, token: string, role: Role } | null>(null);
-    const login = (username: string, token: string, role: Role) => {
-        setIsLoggedIn(true);
-        setUser({ username, token, role });
-        localStorage.setItem("token", JSON.stringify({ username, token, role }));
-    };
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [user, setUser] = useState<{
+    username: string;
+    token: string;
+    role: Role;
+  } | null>(null);
+  const login = (username: string, token: string, role: Role) => {
+    setIsLoggedIn(true);
+    setUser({ username, token, role });
+    localStorage.setItem("token", JSON.stringify({ username, token, role }));
+  };
 
-    const logout = () => {
-        setIsLoggedIn(false);
-        setUser(null);
-        localStorage.removeItem("token");
-    };
+  const logout = () => {
+    setIsLoggedIn(false);
+    setUser(null);
+    localStorage.removeItem("token");
+  };
 
-    useEffect(() => {
-        const stored_state = localStorage.getItem("token");
-        console.log("stored_state " + stored_state);
-        if (stored_state) {
-            setUser(JSON.parse(stored_state));
-            setIsLoggedIn(true);
-        }
-    }, [])
+  useEffect(() => {
+    const stored_state = localStorage.getItem("token");
+    console.log("stored_state " + stored_state);
+    if (stored_state) {
+      setUser(JSON.parse(stored_state));
+      setIsLoggedIn(true);
+    }
+  }, []);
 
-
-    return (
-        <AuthContext.Provider value={{ isLoggedIn, user, login, logout }}>
-            {children}
-        </AuthContext.Provider>
-    );
+  return (
+    <AuthContext.Provider value={{ isLoggedIn, user, login, logout }}>
+      {children}
+    </AuthContext.Provider>
+  );
 };
 
 export const useAuth = () => {
-    const context = useContext(AuthContext);
-    if (!context) {
-        throw new Error('useAuth must be used within an AuthProvider');
-    }
-    return context;
+  const context = useContext(AuthContext);
+  if (!context) {
+    throw new Error("useAuth must be used within an AuthProvider");
+  }
+  return context;
 };
